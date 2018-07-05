257c554baf170be3123f30c70fee85b0a18ff04c
CGPer <37706400+CGPer@users.noreply.github.com>
2018-07-05 16:11:38 +0800

    业务逻辑

--- mini_calc_logic.py
+++ mini_calc_logic.py
@@ -7,9 +7,9 @@ class MyMainWindow(QMainWindow, Ui_Form):
     def __init__(self):
         super(MyMainWindow, self).__init__()
         self.setupUi(self)
-        self.equal.clicked.connect(self.click_equal) #绑定等号按钮和回调函数
+        self.equal.clicked.connect(self.click_equal)     #绑定等号按钮和回调函数
 
-    def click_equal(self):                           #等号按钮的回调函数
+    def click_equal(self):                               #等号按钮的回调函数
         try:
             contain = str(eval(self.show_input.text()))  #获取输入的文本并转换为表达式
             if len(contain) > 12:                        #如果计算结果大于12位
@@ -17,7 +17,7 @@ class MyMainWindow(QMainWindow, Ui_Form):
             else:
                 self.result.setText(contain)             #计算结果小于12位则将计算结果输出结果框
         except:
-            self.result.setText('表达式错误')             #如果表达式无法计算，则报错
+            self.result.setText('表达式错误')              #如果表达式无法计算，则报错
 
 
 if __name__ == '__main__':
